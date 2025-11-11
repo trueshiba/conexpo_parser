da029f81873d9efb409d4f14d5a3e0603ceca33e
trueshiba <107582031+trueshiba@users.noreply.github.com>
2025-11-11 02:30:37 -0500

    functions and formats correctly

--- parser.py
+++ parser.py
@@ -1,23 +1,38 @@
 from bs4 import BeautifulSoup
-import requests
+import csv
+
 
 # Load webpage into file for beautiful soup to parse
-local_file = "exhibitor_list.html"
+local_file = "full_exhibitor_site.html"
 page = open(local_file, encoding="utf-8")
 
 soup = BeautifulSoup(page.read(), "html.parser")
 
-# Take just the exhibitor table
-soup_table = soup.find("div", {"class": "searchResultWrapper"})
-
+# Take just the full exhibitor table and not featured
+soup_table = soup.findAll("table", {"class": "results-table word-wrap"})[1]
 exhibitor_list = soup_table.find_all("a")
 
-csv = []
+out_list = []
 for entry in exhibitor_list:
-    csv.append(entry.get_text().strip())
+    out_list.append(entry.get_text(strip=True))
 
-# Print the first exhibitor
-print(csv[:2])
+# Filter out empty/None entries
+filtered_list = list(filter(None, out_list))
 
-# Print the last exhibitor
-print(csv[-2:])
+bad_str = "\n                                      "
+exhibitors = [str(x).replace(bad_str, " ") for ind, x in enumerate(filtered_list) if ind % 2 == 0]
+booth_num = [x for ind, x in enumerate(filtered_list) if ind % 2 == 1]
+
+print(exhibitors)
+print(f"Number of companies: {len(exhibitors)}")
+
+# with open("output.csv", "w", newline="") as csv_out:
+#     outfile = csv.writer(csv_out)
+
+#     # Columns
+#     outfile.writerow(["Exhibitor", "Booth"])
+
+#     for i in range(0, len(exhibitors))
+
+
+    
