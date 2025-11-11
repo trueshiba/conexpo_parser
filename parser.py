ae8ce667270a9e7693d81eb71b136bb86bb4a8cc
trueshiba <107582031+trueshiba@users.noreply.github.com>
2025-11-10 23:31:40 -0500

    Gets all exhibitors and ids but sometimes included newlines

--- parser.py
+++ parser.py
@@ -1,14 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-AGENT = os.getenv("AGENT")
+# Load webpage into file for beautiful soup to parse
+local_file = "exhibitor_list.html"
+page = open(local_file, encoding="utf-8")
 
-# Get webpage into str
-url = "https://directory.conexpoconagg.com/8_0/explore/exhibitor-gallery.cfm?featured=false"
-headers = {'User-Agent': AGENT}
-response = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.read(), "html.parser")
 
-print(f"First signs of life: {response.text[:200]}")
+# Take just the exhibitor table
+soup_table = soup.find("div", {"class": "searchResultWrapper"})
+
+exhibitor_list = soup_table.find_all("a")
+
+csv = []
+for entry in exhibitor_list:
+    csv.append(entry.get_text().strip())
+
+# Print the first exhibitor
+print(csv[:2])
+
+# Print the last exhibitor
+print(csv[-2:])
